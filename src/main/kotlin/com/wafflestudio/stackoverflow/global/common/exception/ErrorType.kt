beb63c022efa8e68003a344fa586afa7729275eb
moonpiderman <lotsofchuck@gmail.com>
2021-11-23 13:37:31 +0900

    Exception Handler

---
+++ src/main/kotlin/com/wafflestudio/stackoverflow/global/common/exception/ErrorType.kt
@@ -0,0 +1,15 @@
+package com.wafflestudio.stackoverflow.global.common.exception
+
+enum class ErrorType (
+    val code: Int
+) {
+    INVALID_REQUEST(0),
+
+    NOT_ALLOWED(3000),
+
+    DATA_NOT_FOUND(4000),
+
+    CONFLICT(9000),
+
+    SERVER_ERROR(10000)
+}
