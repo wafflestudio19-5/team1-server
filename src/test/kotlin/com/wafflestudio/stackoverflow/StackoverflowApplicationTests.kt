ca4c9aecae801085a5f625d9fe45322f6b1e31eb
moonpiderman <lotsofchuck@gmail.com>
2021-11-23 11:10:48 +0900

    initailize

---
+++ src/test/kotlin/com/wafflestudio/stackoverflow/StackoverflowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wafflestudio.stackoverflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StackoverflowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
