eaa4a52a44416de18484f354583dd6896f1ef2d6
moonpiderman <lotsofchuck@gmail.com>
2021-12-09 16:29:31 +0900

    Add: Question Modeling

--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/question/model/Question.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/question/model/Question.kt
@@ -1,9 +1,29 @@
 package com.wafflestudio.waffleoverflow.domain.question.model
 
+import com.wafflestudio.waffleoverflow.domain.answer.model.Answer
+import com.wafflestudio.waffleoverflow.domain.comment.model.Comment
 import com.wafflestudio.waffleoverflow.domain.model.BaseTimeEntity
+import com.wafflestudio.waffleoverflow.domain.user.model.User
+import com.wafflestudio.waffleoverflow.domain.vote.model.Vote
+import javax.persistence.CascadeType
 import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
 @Table(name = "question")
-class Question() : BaseTimeEntity()
+class Question(
+    @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    val user: User? = null,
+
+    @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    var answers: MutableList<Answer> = mutableListOf(),
+
+    @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    var votes: MutableList<Vote> = mutableListOf(),
+
+    @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    var comments: MutableList<Comment> = mutableListOf(),
+) : BaseTimeEntity()
