6351ecd930a36569b35b8d8681e8726f0904b29d
moonpiderman <lotsofchuck@gmail.com>
2021-12-09 16:13:50 +0900

    Add: add Columns

--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/user/model/User.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/user/model/User.kt
@@ -1,9 +1,16 @@
 package com.wafflestudio.waffleoverflow.domain.user.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.wafflestudio.waffleoverflow.domain.answer.model.Answer
 import com.wafflestudio.waffleoverflow.domain.model.BaseEntity
+import com.wafflestudio.waffleoverflow.domain.question.model.Question
+import com.wafflestudio.waffleoverflow.domain.vote.model.Vote
+import com.wafflestudio.waffleoverflow.domain.comment.model.Comment
+import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.OneToMany
 import javax.persistence.Table
 import javax.validation.constraints.NotBlank
 
@@ -21,5 +28,17 @@ class User(
     var password: String,
 
     @JsonIgnore
-    val authorities: String = "User"
+    val authorities: String = "User",
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    var questions: MutableList<Question> = mutableListOf(),
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    var answers: MutableList<Answer> = mutableListOf(),
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    var votes: MutableList<Vote> = mutableListOf(),
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    var comments: MutableList<Comment> = mutableListOf(),
 ) : BaseEntity()
