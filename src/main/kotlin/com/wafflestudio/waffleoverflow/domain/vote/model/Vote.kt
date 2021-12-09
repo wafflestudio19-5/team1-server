4fe67828227dfe3341259231caa08061f1e56970
moonpiderman <lotsofchuck@gmail.com>
2021-12-09 15:46:28 +0900

    Fix: Set entity, table

--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/vote/model/Vote.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/vote/model/Vote.kt
@@ -1,5 +1,9 @@
 package com.wafflestudio.waffleoverflow.domain.vote.model
 
 import com.wafflestudio.waffleoverflow.domain.model.BaseTimeEntity
+import javax.persistence.Entity
+import javax.persistence.Table
 
+@Entity
+@Table(name = "vote")
 class Vote() : BaseTimeEntity()
