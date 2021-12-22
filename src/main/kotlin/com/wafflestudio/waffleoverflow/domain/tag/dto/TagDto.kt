e0f4171e9ca5d451397c3307847d7f642c67eac3
Haneul <caelum02@snu.ac.kr>
2021-12-22 16:53:59 +0900

    Feat: Add TagDto (Cherrypick)

--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/tag/dto/TagDto.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/tag/dto/TagDto.kt
@@ -1,3 +1,17 @@
 package com.wafflestudio.waffleoverflow.domain.tag.dto
 
-class TagDto
+import com.wafflestudio.waffleoverflow.domain.tag.model.Tag
+
+class TagDto {
+    data class Wrapper(
+        val id: Long,
+        val name: String,
+        val intro: String,
+    ) {
+        constructor(tag: Tag) : this(
+            tag.id,
+            tag.name,
+            tag.intro
+        )
+    }
+}
