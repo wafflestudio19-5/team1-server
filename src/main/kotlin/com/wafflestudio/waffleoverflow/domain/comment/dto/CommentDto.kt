3931575a2372a47d372c29750bf8be42bd77ef4a
Haneul <caelum02@snu.ac.kr>
2021-12-22 14:42:29 +0900

    Feat: Add CommentDto.kt

---
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/comment/dto/CommentDto.kt
@@ -0,0 +1,24 @@
+package com.wafflestudio.waffleoverflow.domain.comment.dto
+
+import com.wafflestudio.waffleoverflow.domain.comment.model.Comment
+import com.wafflestudio.waffleoverflow.domain.user.dto.UserDto
+
+class CommentDto (
+
+    ){
+    data class Wrapper(
+        val id: Long,
+        val user: UserDto.Response,
+        val body: String,
+        val questionId: Long?,
+        val answerId: Long?,
+    ) {
+        constructor(comment: Comment) : this(
+            comment.id,
+            UserDto.Response(comment.user),
+            comment.body,
+            comment.question?.id,
+            comment.answer?.id
+        )
+    }
+}
