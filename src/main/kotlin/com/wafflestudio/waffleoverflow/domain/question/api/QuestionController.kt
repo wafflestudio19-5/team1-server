b48b3f551635f6767de7768b8b306f38697194d7
Haneul <caelum02@snu.ac.kr>
2021-12-22 21:33:42 +0900

    Feat: add getQuestion func prototype

--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/question/api/QuestionController.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/question/api/QuestionController.kt
@@ -1,8 +1,22 @@
 package com.wafflestudio.waffleoverflow.domain.question.api
 
+import com.wafflestudio.waffleoverflow.domain.question.dto.QuestionDto
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/question")
-class QuestionController
+class QuestionController() {
+    @GetMapping("/{question_id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getQuestion(
+        @Valid @RequestBody body: RequestBody,
+    ): QuestionDto.Response {
+        return QuestionDto.Response(null)
+    }
+}
