708f274a26218ada373400d275328687baaa2af0
caelum02 <caelum02@snu.ac.kr>
2021-12-22 22:09:02 +0900

    Add: QuestionController.getQuestion completed

--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/question/api/QuestionController.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/question/api/QuestionController.kt
@@ -1,8 +1,10 @@
 package com.wafflestudio.waffleoverflow.domain.question.api
 
 import com.wafflestudio.waffleoverflow.domain.question.dto.QuestionDto
+import com.wafflestudio.waffleoverflow.domain.question.service.QuestionService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -11,12 +13,17 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/question")
-class QuestionController() {
+class QuestionController(
+    private val questionService: QuestionService
+) {
     @GetMapping("/{question_id}")
     @ResponseStatus(HttpStatus.OK)
     fun getQuestion(
         @Valid @RequestBody body: RequestBody,
+        @PathVariable question_id: Long
     ): QuestionDto.Response {
-        return QuestionDto.Response(null)
+        return QuestionDto.Response(
+            questionService.findById(question_id)
+        )
     }
 }
--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/question/service/QuestionService.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/question/service/QuestionService.kt
@@ -1,9 +1,16 @@
 package com.wafflestudio.waffleoverflow.domain.question.service
 
+import com.wafflestudio.waffleoverflow.domain.question.exception.QuestionNotFoundException
+import com.wafflestudio.waffleoverflow.domain.question.model.Question
 import com.wafflestudio.waffleoverflow.domain.question.repository.QuestionRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class QuestionService(
     private val questionRepository: QuestionRepository,
-)
+) {
+    fun findById(id: Long): Question {
+        return questionRepository.findByIdOrNull(id) ?: throw QuestionNotFoundException("Question $id does not exist")
+    }
+}
