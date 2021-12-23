4a6f1899e1235326107bf0453fdc5196babc0c71
caelum02 <caelum02@snu.ac.kr>
2021-12-23 23:24:59 +0900

    Feat: add ListResponse.kt

---
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/global/common/dto/ListResponse.kt
@@ -0,0 +1,11 @@
+package com.wafflestudio.waffleoverflow.global.common.dto
+
+data class ListResponse<T>(
+    val results: List<T>,
+    val count: Int
+) {
+    constructor(list: List<T>?) : this(
+        list.orEmpty(),
+        list?.size ?: 0
+    )
+}
