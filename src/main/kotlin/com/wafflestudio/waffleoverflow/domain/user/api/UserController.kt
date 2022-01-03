84cc6ec647a42f2848f130a765de7979b0e1417f
moonpiderman <lotsofchuck@gmail.com>
2022-01-03 15:08:24 +0900

    Add: User Me API

--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/user/api/UserController.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/user/api/UserController.kt
@@ -1,17 +1,21 @@
 package com.wafflestudio.waffleoverflow.domain.user.api
 
 import com.wafflestudio.waffleoverflow.domain.user.dto.UserDto
+import com.wafflestudio.waffleoverflow.domain.user.model.User
 import com.wafflestudio.waffleoverflow.domain.user.service.UserService
+import com.wafflestudio.waffleoverflow.global.auth.CurrentUser
 import com.wafflestudio.waffleoverflow.global.auth.jwt.JwtTokenProvider
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
 
+@ResponseStatus
 @RestController
 @RequestMapping("/api/user")
 class UserController(
@@ -19,12 +23,22 @@ class UserController(
     private val jwtTokenProvider: JwtTokenProvider,
 ) {
     @PostMapping("/signup/")
+    @ResponseStatus(HttpStatus.OK)
     fun signup(
         @Valid @RequestBody signupRequest: UserDto.SignupRequest,
         response: HttpServletResponse
-    ): ResponseEntity<UserDto.Response> {
+    ): UserDto.Response {
         val user = userService.signup(signupRequest)
         response.addHeader("Authentication", jwtTokenProvider.generateToken(user.email))
-        return ResponseEntity(UserDto.Response(user), HttpStatus.OK)
+        return UserDto.Response(user)
+    }
+
+    @GetMapping("/me/")
+    @ResponseStatus(HttpStatus.OK)
+    fun myInfo(
+        @CurrentUser user: User,
+    ): UserDto.Response {
+        val thisUser = userService.myInfo(user)
+        return UserDto.Response(thisUser)
     }
 }
