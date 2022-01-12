2a4bd784c7e45b98ec915cf801004c38daf53d83
moonpiderman <lotsofchuck@gmail.com>
2022-01-12 19:33:05 +0900

    Add: Test

--- src/test/kotlin/com/wafflestudio/waffleoverflow/WaffleoverflowApplicationTests.kt
+++ src/test/kotlin/com/wafflestudio/waffleoverflow/WaffleoverflowApplicationTests.kt
@@ -1,12 +1,11 @@
 package com.wafflestudio.waffleoverflow
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class WaffleoverflowApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+    // @Test
+    // fun contextLoads() {
+    // }
 }
