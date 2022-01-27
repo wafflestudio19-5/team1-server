0b5235a17fcdf012e3abf22371047770c5d4fae9
yg-moon <yg-moon@snu.ac.kr>
2022-01-27 14:35:41 +0900

    Refactor: minor changes (Answer domain)

--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/answer/exception/AnswerNotFoundException.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/answer/exception/AnswerNotFoundException.kt
@@ -3,4 +3,5 @@ package com.wafflestudio.waffleoverflow.domain.answer.exception
 import com.wafflestudio.waffleoverflow.global.common.exception.DataNotFoundException
 import com.wafflestudio.waffleoverflow.global.common.exception.ErrorType
 
-class AnswerNotFoundException(detail: String) : DataNotFoundException(errorType = ErrorType.DATA_NOT_FOUND, detail)
+class AnswerNotFoundException(detail: String) :
+    DataNotFoundException(errorType = ErrorType.DATA_NOT_FOUND, detail)
