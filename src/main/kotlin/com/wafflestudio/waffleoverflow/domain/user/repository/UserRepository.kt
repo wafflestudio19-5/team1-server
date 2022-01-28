1eea0090f64751d09ad42c11631c562ba47969bd
yg-moon <yg-moon@snu.ac.kr>
2022-01-28 12:07:08 +0900

    Fix: filter deleted users from the list

--- src/main/kotlin/com/wafflestudio/waffleoverflow/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/wafflestudio/waffleoverflow/domain/user/repository/UserRepository.kt
@@ -1,10 +1,13 @@
 package com.wafflestudio.waffleoverflow.domain.user.repository
 
 import com.wafflestudio.waffleoverflow.domain.user.model.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long?> {
     fun findByEmail(email: String): User?
     fun existsUserByUsername(name: String): Boolean
     fun existsUserByEmail(email: String): Boolean
+    fun findAllByDeletedIsFalse(pageable: Pageable): Page<User>
 }
